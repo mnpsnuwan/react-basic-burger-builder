3fb75cfbf3152caa32583cad257a1f379c106940
mnpsnuwan <nuwanp124@gmail.com>
2023-10-13 15:14:43 -0700

    IM:DEV:React Basic Burger Builder - Adding prop types and Starting the Burger Component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Layout from "./components/Layout/Layout";
-import BurgerBuilder from "./components/BurgerBuilder/BurgerBuilder";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 function App() {
   return (
-      <div className="App">
+    <div className="App">
         <Layout>
-          <BurgerBuilder />
+            <BurgerBuilder />
         </Layout>
-      </div>
+    </div>
   );
 }
 
