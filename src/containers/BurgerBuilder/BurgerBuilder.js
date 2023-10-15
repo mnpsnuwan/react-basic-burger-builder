7d045928728c6c5f79684472f6b53faf93791954
mnpsnuwan <nuwanp124@gmail.com>
2023-10-15 14:11:56 -0700

    IM:DEV:React Basic Burger Builder - Adding Backdrop Components.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -77,6 +77,10 @@ class BurgerBuilder extends Component{
         this.setState({purchasing: true})
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false})
+    }
+
     render() {
 
         const disableInfo = {
@@ -90,7 +94,7 @@ class BurgerBuilder extends Component{
 
         return (
             <Auc>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
