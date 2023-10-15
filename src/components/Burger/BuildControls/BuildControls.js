05c5dd0b3cfc4980651e97a0e95792bab275f0e9
mnpsnuwan <nuwanp124@gmail.com>
2023-10-14 17:05:31 -0700

    IM:DEV:React Basic Burger Builder - Adding Price and Order Button.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,12 +11,14 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price : <strong>${props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
             <BuildControl
                 key={ctrl.label}
                 label={ctrl.label}
                 added={() => props.ingredientAdded(ctrl.type)}
-                removed={props.ingredientRemoved}/>
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}/>
         ))}
     </div>
 );
