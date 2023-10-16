c490cfe9d0e7b4f86c64c8a868c02d3cb51a7afe
mnpsnuwan <nuwanp124@gmail.com>
2023-10-16 11:22:27 -0700

    IM:DEV:React Basic Burger Builder - Adding Logo & Reusable Navigation Items.

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import classes from './Toolbar.css';
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
-const toolbar = (props) => (
+const toolbar = () => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>
-            {props.children}
+            <NavigationItems />
         </nav>
     </header>
 );
