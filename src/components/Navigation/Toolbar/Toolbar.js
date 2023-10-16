76811bcc215c3fdb91ab5bd776a5d7e7ab9b7b05
mnpsnuwan <nuwanp124@gmail.com>
2023-10-16 12:30:12 -0700

    IM:DEV:React Basic Burger Builder - Responsive Adjustment Further.

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -11,7 +11,7 @@ const toolbar = () => (
         <div className={classes.Logo}>
             <Logo />
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
