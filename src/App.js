2a47d0297c6ec9068e46e03db72f335f17fa284b
mnpsnuwan <nuwanp124@gmail.com>
2023-10-28 16:18:12 -0700

    IM:DEV:React Basic Burger Builder - Building the Checkout Container.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 
 function App() {
   return (
     <div className="App">
         <Layout>
             <BurgerBuilder />
+            <Checkout />
         </Layout>
     </div>
   );
