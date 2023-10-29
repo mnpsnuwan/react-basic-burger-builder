bc96feaf9a716177c35f3fe653a85502e8bde102
mnpsnuwan <nuwanp124@gmail.com>
2023-10-29 16:11:03 -0700

    IM:DEV:React Basic Burger Builder - Implementing Navigation Links.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Route, Switch} from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
 
 function App() {
   return (
@@ -11,8 +12,7 @@ function App() {
         <Layout>
             <Switch>
                 <Route path="/" exact component={BurgerBuilder} />
-                <Route path="/checkout" component={Checkout} />
-                <Route path="/orders" component={Checkout} />
+                <Route path="/orders" component={Orders} />
             </Switch>
         </Layout>
     </div>
