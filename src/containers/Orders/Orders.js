a63473c0dcdffbc9e89bbdf51e05b9a31cd690bb
mnpsnuwan <nuwanp124@gmail.com>
2023-10-29 20:30:04 -0700

    IM:DEV:React Basic Burger Builder - Fetching Orders.

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,32 @@
 import React, {Component} from "react";
 import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends Component{
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                // console.log(res.data);
+                const fetchOrders = [];
+                for (let key in res.data) {
+                    fetchOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                console.log(fetchOrders);
+                this.setState({loading: false, orders: fetchOrders})
+            })
+            .catch(err => {
+                this.setState({loading: false})
+            });
+    }
 
     render() {
         return (
@@ -13,4 +38,4 @@ class Orders extends Component{
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
